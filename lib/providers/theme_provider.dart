3d9c8d613363e9500d93ef79e003e8fc9ed5a19e
mendesjaaziel <161533490+mendesjaaziel@users.noreply.github.com>
2026-05-21 11:30:12 -0300

    Add ThemeProvider for proper theme management

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  bool _isDarkMode = false;
+  late SharedPreferences _prefs;
+  
+  bool get isDarkMode => _isDarkMode;
+  
+  ThemeMode get themeMode => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
+
+  Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+    _isDarkMode = _prefs.getBool('isDarkMode') ?? false;
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    await _prefs.setBool('isDarkMode', _isDarkMode);
+    notifyListeners();
+  }
+
+  Future<void> setDarkMode(bool value) async {
+    _isDarkMode = value;
+    await _prefs.setBool('isDarkMode', value);
+    notifyListeners();
+  }
+}
